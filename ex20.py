deec4acc19bd80968ff91453e638f5a19ec32bd9
Samuel de Sousa Sidrone <samuelsousasidrone@gmail.com>
2024-10-21 16:56:27 -0300

    ex20 update

--- ex20.py
+++ ex20.py
@@ -1,6 +1,6 @@
-ano = int(input('Qual ano você quer analisar? '))
+ano = int(input('Qual ano você quer analisar? ')) # pede o ano
 
-if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0: #verifica se e bissexto ou não
     print('O ano {} é bissexto'.format(ano))
 else:
     print('Esse ano {} não e bissexto'.format(ano))
